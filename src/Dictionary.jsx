63b7f3ad528393643061a1e69417680b3e44e18b
gusacci <106109916+gusacci@users.noreply.github.com>
2022-07-08 16:15:29 +0300

    Added meanings

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 const Dictionary = () => {
   const [keyword, setKeyword] = useState();
+  const [results, setResults] = useState(null);
   const handleKeywordChange = (event) => {
     setKeyword(event.target.value);
   };
   const handleResponse = (response) => {
-    console.log(response);
+    setResults(response.data[0]);
   };
   const search = (event) => {
     event.preventDefault();
@@ -21,6 +23,7 @@ const Dictionary = () => {
         <input type="text" onChange={handleKeywordChange} />
         <button type="submit">Search</button>
       </form>
+      <Results results={results} />
     </div>
   );
 };
---
+++ src/Meaning.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Meaning = (props) => {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+
+            <p>
+              {" "}
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Meaning;
