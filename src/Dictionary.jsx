c1c6ef3fe14e053119de931795c76c2bcb75bb01
gusacci <106109916+gusacci@users.noreply.github.com>
2022-07-10 17:53:07 +0300

    added photos

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 const Dictionary = () => {
   const [keyword, setKeyword] = useState();
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
   const handleKeywordChange = (event) => {
     setKeyword(event.target.value);
   };
   const handleResponse = (response) => {
     setResults(response.data[0]);
   };
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   const search = (event) => {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let pexelsKey = `563492ad6f917000010000011f2c9dccb24f4e75b318a27c0a08babd`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   };
   return (
     <div className="Dictionary">
@@ -27,10 +38,23 @@ const Dictionary = () => {
         </form>
       </div>
       <Results results={results} />
+      <Photos photos={photos} />
       <p className="footer">
-        Coded by{" "}
+        This project was coded by
         <a href="/" target="_blank" rel="noreferrer">
           Anastasia Gusak
+        </a>
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/gusacci/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+          Netlify
         </a>
       </p>
     </div>
